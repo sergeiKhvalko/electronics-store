480f0d486944dadee33223f8a343ec296b522c21
Sergei Khvalko <falko.serega2016@yandex.ru>
2022-05-03 20:33:53 +0300

    init redux

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 import "antd/dist/antd.css";
 
+import store from './redux';
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router>
-    <App />
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
 );
 
 reportWebVitals();
