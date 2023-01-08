e4557ecc15165749b878929cdedbd6508a1796fc
sergei khvalko <falko.serega2016@yandex.ru>
2023-01-08 23:57:07 +0300

    feat: added CategoryList per page

--- src/components/category/CategoryList.js
+++ src/components/category/CategoryList.js
@@ -1,8 +1,41 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { getCategories } from "../../functions/category";
 
 const CategoryList = () => {
+	const [categories, setCategories] = useState([]);
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		setLoading(true);
+		getCategories().then((cat) => {
+			setCategories(cat.data);
+			setLoading(false)
+		});
+	}, []);
+
+	const showCategories = () => {
+		 return categories.map((cat) => (
+			<div
+				key={cat._id}
+				className="col btn btn-outlined-primary btn-lg btn-block btn-raised m-3"
+			>
+				<Link to={`/category/${cat.slug}`}>{cat.name}</Link>
+			</div>
+		));
+	}
+
 	return (
-		<div>CategoryList</div>
+		<div className="container">
+			<div className="row">
+				{loading ? (
+					<h4 className="text-center text-danger">Loading...</h4>
+				) : (
+					showCategories()
+				)}
+			</div>
+		</div>
 	)
 }
 
-export default CategoryList
+export default CategoryList;
