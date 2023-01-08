4641a515c0a3ddf29a7b03bdbb6b33f928ea654c
sergei khvalko <falko.serega2016@yandex.ru>
2023-01-09 00:15:24 +0300

    feat: added SubList per page

--- src/components/sub/SubList.js
+++ src/components/sub/SubList.js
@@ -1,8 +1,41 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { getSubs } from "../../functions/sub";
 
 const SubList = () => {
-	return (
-		<div>SubList</div>
-	)
-}
+	const [subs, setSubs] = useState([]);
+	const [loading, setLoading] = useState(false);
 
-export default SubList
+	useEffect(() => {
+		setLoading(true);
+		getSubs().then((sub) => {
+			setSubs(sub.data);
+			setLoading(false);
+		});
+	}, [])
+
+	const showSubs = () => {
+		return subs.map((s) => (
+			<div 
+				key={s._id}
+				className="col btn btn-outlined-primary btn-lg btn-block btn-raised m-3"
+			>
+				<Link to={`/sub/${s.slug}`}>{s.name}</Link>
+			</div>
+		));
+	}
+
+	return (
+		<div className="container">
+			<div className="row">
+				{loading ? (
+					<h4 className="text-center text-danger">Loading...</h4>
+				) : (
+					showSubs()
+				)}
+			</div>
+		</div>
+	);
+};
+
+export default SubList;
