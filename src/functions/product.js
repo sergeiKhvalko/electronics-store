80fb6b546ae3943d3b8711d08f713c09c5373ca3
sergei khvalko <falko.serega2016@yandex.ru>
2023-01-24 16:58:32 +0300

    fix: correct body in productStar

--- src/functions/product.js
+++ src/functions/product.js
@@ -46,9 +46,9 @@ export const updateProduct = async (slug, product, authtoken) =>
 		},
 	});
 
-	export const productStar = async (productId, newRating, authtoken) =>
+	export const productStar = async (productId, star, authtoken) =>
 		await axios.put(`${process.env.REACT_APP_API}/product/star/${productId}`,
-			{ newRating },
+			{ star },
 			{
 				headers: {
 					authtoken,
